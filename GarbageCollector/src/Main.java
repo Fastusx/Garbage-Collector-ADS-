2edeefd005c354403b8a50a132d3a7272fba2991
Arthur dos Santos Lima <83731694+Fastusx@users.noreply.github.com>
2025-01-28 14:17:24 -0300

    Update Main.java

--- GarbageCollector/src/Main.java
+++ GarbageCollector/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         System.out.println((runtime.totalMemory() - runtime.freeMemory())/ MB); //memória utilizada pela aplicação (Em Megas)
     }
     public static void main(String[] args) {
-    Contatos[] contatos = new Contatos[1000]; // Criação de n espaços "Contato"
+    Contatos[] contatos = new Contatos[1000000]; // Criação de n espaços "Contato"
     Contatos contato; 
 //Cada espaço em contatos, agora vai receber seus respéctivos valores
     for (int i=0; i< contatos.length; i++){
@@ -15,11 +15,24 @@ public class Main {
         // MBs usados sem o Garbage Colector
         System.out.println("Contatos criados");
         ObterMemoriaUsada();
-        contatos = null;
+        
+        contatos = null; //atribúi null á todas as instâncias contato
+
+        
         Runtime.getRuntime().gc(); // ativação do Garbage Collector
         // MBs usados com o Garbage Collector
         System.out.println("Contatos removidos da memória");
         ObterMemoriaUsada();
+
+    
+    
+    
+    
+    
+       // for (int i = 0; i < contatos.length; i = i + 2){
+            
+         //   contatos[i] = null;
+        //}
     }
 
 }
